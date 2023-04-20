6caec3e797bd438b0751ef16577624074c99b892
Aidan Williams <aidanw4@gmail.com>
2023-04-20 13:49:24 +0700

    add progress sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,7 +3,38 @@ import SideBarCSS from './SideBar.module.css'
 
 const SideBar = () => {
   return (
-    <div className={SideBarCSS.container}>SideBar</div>
+    <div className={SideBarCSS.container}>
+        <div className={SideBarCSS.steps}>
+            <div className={SideBarCSS.step}>
+                <span className={SideBarCSS.stepNum}>1</span>
+                <div className={SideBarCSS.stepInfo}>
+                    <span>Step 1</span>
+                    <h3>YOUR INFO</h3>
+                </div>
+            </div>
+            <div className={SideBarCSS.step}>
+                <span className={SideBarCSS.stepNum}>2</span>
+                <div className={SideBarCSS.stepInfo}>
+                    <span>Step 2</span>
+                    <h3>SELECT PLAN</h3>
+                </div>
+            </div>
+            <div className={SideBarCSS.step}>
+                <span className={SideBarCSS.stepNum}>3</span>
+                <div className={SideBarCSS.stepInfo}>
+                    <span>Step 3</span>
+                    <h3>ADD-ONS</h3>
+                </div>
+            </div>
+            <div className={SideBarCSS.step}>
+                <span className={SideBarCSS.stepNum}>4</span>
+                <div className={SideBarCSS.stepInfo}>
+                    <span>Step 4</span>
+                    <h3>SUMMARY</h3>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
